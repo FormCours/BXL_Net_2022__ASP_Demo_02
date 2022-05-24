fdc96ba47e8feb973af4cbf56cc11888ef492ab3
Aurélien S <aurelien.strimelle@bstorm.be>
2022-05-24 15:30:26 +0200

    Demo custom TagHelper

---
+++ ASP_MVC_02_Formulaire/Helpers/CustomTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ASP_MVC_02_Formulaire.Helpers
+{
+
+    [HtmlTargetElement("hello-world")]
+    public class CustomTagHelper : TagHelper
+    {
+        public string Name { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "span";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            output.Content.SetContent("Hello " + Name);
+        }
+    }
+}
